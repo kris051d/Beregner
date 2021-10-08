6b2b59899589ecc8883f84718e6964f7dc734cb4
kris051d <kris051d@edu.easj.dk>
2021-10-08 14:01:17 +0200

    +Dyr+file

---
+++ src/com/company/CreateFile.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.io.File;  // Import the File class
+import java.io.IOException;  // Import the IOException class to handle err
+
+public class CreateFile {
+  public static void main(String[] args) {
+    try {
+      File myObj = new File("David.txt");
+      if (myObj.createNewFile()) {
+        System.out.println("File created: " + myObj.getName());
+      } else {
+        System.out.println("File already exists.");
+      }
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+  }
+}
